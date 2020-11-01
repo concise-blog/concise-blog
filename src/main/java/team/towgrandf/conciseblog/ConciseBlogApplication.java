ba2c484285a65b014937ef25c8c207ce320abeff
wangshuo <wangshuoszqj@gmail.com>
2020-11-01 13:59:01 +0800

    add something

--- src/main/java/team/towgrandf/conciseblog/ConciseBlogApplication.java
+++ src/main/java/team/towgrandf/conciseblog/ConciseBlogApplication.java
@@ -3,6 +3,9 @@ package team.towgrandf.conciseblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 博客启动类
+ */
 @SpringBootApplication
 public class ConciseBlogApplication {
 
