e64be48e155888cc874ddcf7a2074fdcb1575be9
jssd <1624022009@qq.com>
2020-11-01 13:32:32 +0800

    build(global): init project

---
+++ src/main/java/team/towgrandf/conciseblog/ConciseBlogApplication.java
@@ -0,0 +1,13 @@
+package team.towgrandf.conciseblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciseBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConciseBlogApplication.class, args);
+    }
+
+}
